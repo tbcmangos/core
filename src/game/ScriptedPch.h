af594d33976ce5a4202cfe1780c9e2ce259ee3c6
tbcmangos <tbc.mangos@gmail.com>
2020-07-31 20:41:01 +0800

    Scripts : disable dllmain

--- src/game/ScriptedPch.h
+++ src/game/ScriptedPch.h
@@ -35,16 +35,17 @@
 #include "ScriptedInstance.h"
 #include "CombatAI.h"
 #include "PassiveAI.h"
+#include "Config/Config.h"
 
 #ifdef WIN32
 #include <windows.h>
-BOOL APIENTRY DllMain( HANDLE hModule,
+/*BOOL APIENTRY DllMain( HANDLE hModule,
 DWORD  ul_reason_for_call,
 LPVOID lpReserved
 )
 {
     return true;
-}
+}*/
 #endif
 
 #endif
