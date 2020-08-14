1071c5f8806a863873a77de0fb15129e177dfe78
tbcmangos <tbc.mangos@gmail.com>
2020-08-14 22:24:53 +0800

    Update Reference.h

--- src/framework/Utilities/LinkedReference/Reference.h
+++ src/framework/Utilities/LinkedReference/Reference.h
@@ -1,7 +1,8 @@
 /*
- * Copyright (C) 2005-2008 MaNGOS <http://getmangos.com/>
- * Copyright (C) 2008 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2009-2011 MaNGOSZero <https://github.com/mangos/zero>
+ * Copyright (C) 2011-2016 Nostalrius <https://nostalrius.org>
+ * Copyright (C) 2016-2017 Elysium Project <https://github.com/elysium-project>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -10,28 +11,31 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef HELLGROUND_REFERENCE_H
-#define HELLGROUND_REFERENCE_H
+#ifndef _REFERENCE_H
+#define _REFERENCE_H
 
 #include "Utilities/LinkedList.h"
-#include "Log.h"
 
 //=====================================================
 
-template <class TO, class FROM> class Reference : public LinkedListElement
+template<class TO, class FROM>
+class Reference : public LinkedListElement
 {
     private:
-        TO* iRefTo;
-        FROM* iRefFrom;
+
+        TO* iRefTo = nullptr;
+        FROM* iRefFrom = nullptr;
+
     protected:
+
         // Tell our refTo (target) object that we have a link
         virtual void targetObjectBuildLink() = 0;
 
@@ -40,17 +44,19 @@ template <class TO, class FROM> class Reference : public LinkedListElement
 
         // Tell our refFrom (source) object, that the link is cut (Target destroyed)
         virtual void sourceObjectDestroyLink() = 0;
+
     public:
-        Reference() { iRefTo = NULL; iRefFrom = NULL; }
+
         virtual ~Reference() {}
 
         // Create new link
-        inline void link(TO* toObj, FROM* fromObj)
+        void link(TO* toObj, FROM* fromObj)
         {
-            ASSERT(fromObj);                                // fromObj MUST not be NULL
-            if(isValid())
+            assert(fromObj);                                // fromObj MUST not be nullptr
+            if (isValid())
                 unlink();
-            if(toObj != NULL)
+
+            if (toObj != nullptr)
             {
                 iRefTo = toObj;
                 iRefFrom = fromObj;
@@ -60,36 +66,39 @@ template <class TO, class FROM> class Reference : public LinkedListElement
 
         // We don't need the reference anymore. Call comes from the refFrom object
         // Tell our refTo object, that the link is cut
-        inline void unlink() { targetObjectDestroyLink(); delink(); iRefTo = NULL; iRefFrom = NULL; }
+        void unlink()
+        {
+            targetObjectDestroyLink();
+            delink();
+            iRefTo = nullptr;
+            iRefFrom = nullptr;
+        }
 
         // Link is invalid due to destruction of referenced target object. Call comes from the refTo object
         // Tell our refFrom object, that the link is cut
-        inline void invalidate()                            // the iRefFrom MUST remain!!
+        void invalidate()                                   // the iRefFrom MUST remain!!
         {
-            sourceObjectDestroyLink(); delink(); iRefTo = NULL;
+            sourceObjectDestroyLink();
+            delink();
+            iRefTo = nullptr;
         }
 
-        inline bool isValid() const                         // Only check the iRefTo
+        bool isValid() const                                // Only check the iRefTo
         {
-            return iRefTo != NULL;
+            return iRefTo != nullptr;
         }
 
-        Reference<TO,FROM>       * next()       { return((Reference<TO,FROM>       *) LinkedListElement::next()); }
-        Reference<TO,FROM> const * next() const { return((Reference<TO,FROM> const *) LinkedListElement::next()); }
-        Reference<TO,FROM>       * prev()       { return((Reference<TO,FROM>       *) LinkedListElement::prev()); }
-        Reference<TO,FROM> const * prev() const { return((Reference<TO,FROM> const *) LinkedListElement::prev()); }
+        Reference<TO,FROM>      * next()       { return ((Reference<TO, FROM>      *) LinkedListElement::next()); }
+        Reference<TO,FROM> const* next() const { return ((Reference<TO, FROM> const*) LinkedListElement::next()); }
+        Reference<TO,FROM>      * prev()       { return ((Reference<TO, FROM>      *) LinkedListElement::prev()); }
+        Reference<TO,FROM> const* prev() const { return ((Reference<TO, FROM> const*) LinkedListElement::prev()); }
 
-        Reference<TO,FROM>       * nocheck_next()       { return((Reference<TO,FROM>       *) LinkedListElement::nocheck_next()); }
-        Reference<TO,FROM> const * nocheck_next() const { return((Reference<TO,FROM> const *) LinkedListElement::nocheck_next()); }
-        Reference<TO,FROM>       * nocheck_prev()       { return((Reference<TO,FROM>       *) LinkedListElement::nocheck_prev()); }
-        Reference<TO,FROM> const * nocheck_prev() const { return((Reference<TO,FROM> const *) LinkedListElement::nocheck_prev()); }
+        TO* operator->() const { return iRefTo; }
+        TO* getTarget() const { return iRefTo; }
 
-        inline TO* operator ->() const { return iRefTo; }
-        inline TO* getTarget() const { return iRefTo; }
-
-        inline FROM* getSource() const { return iRefFrom; }
+        FROM* getSource() const { return iRefFrom; }
 };
 
 //=====================================================
-#endif
 
+#endif
