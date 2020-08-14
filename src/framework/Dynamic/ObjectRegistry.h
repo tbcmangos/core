9893d5241fc0da11331e809f0bb81eab78fa7b8e
tbcmangos <tbc.mangos@gmail.com>
2020-08-14 22:24:44 +0800

    Update ObjectRegistry.h

--- src/framework/Dynamic/ObjectRegistry.h
+++ src/framework/Dynamic/ObjectRegistry.h
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2005-2008 MaNGOS <http://getmangos.com/>
- * Copyright (C) 2008 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
+ *
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#ifndef HELLGROUND_OBJECTREGISTRY_H
-#define HELLGROUND_OBJECTREGISTRY_H
+#ifndef MANGOS_OBJECTREGISTRY_H
+#define MANGOS_OBJECTREGISTRY_H
 
 #include "Platform/Define.h"
 #include <unordered_map>
